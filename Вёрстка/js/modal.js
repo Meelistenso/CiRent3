839c670fe32859da2168af9d3166dbe3531ed8e2
Meelistenso <meelistenso@gmail.com>
2017-05-20 15:50:33 +0300

    product.html

---
+++ Вёрстка/js/modal.js
@@ -0,0 +1,12 @@
+			$("#close_profile_modal").click( function(){
+				$(".profile-modal").css("display", "none");
+			});
+			$("#open_profile_modal").click( function(){
+				$(".profile-modal").css("display", "flex");
+			});
+			$("#close_cart_modal").click( function(){
+				$(".cart-modal").css("display", "none");
+			});
+			$("#open_cart_modal").click( function(){
+				$(".cart-modal").css("display", "flex");
+			});
